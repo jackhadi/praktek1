683be8d831f2b095fd6ef946f2cd02471896f7d9
hadi <jackhadi@gmail.com>
2018-04-05 14:45:48 +0800

    iinstance object

--- src/praktek1/AnjingAksi.java
+++ src/praktek1/AnjingAksi.java
@@ -5,6 +5,11 @@ package praktek1;
 public class AnjingAksi {
     public static void main(String[] args) {
         
+        anjing A = new anjing();
+        anjing B = new anjing();
+        anjing C = new anjing();
+        
+        
     }
     
 }
