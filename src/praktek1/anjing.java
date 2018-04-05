64aec1f1027ee8390452b95b36f3df1aa3aee504
hadi <jackhadi@gmail.com>
2018-04-05 14:39:08 +0800

    membuat atribut dan behavior

--- src/praktek1/anjing.java
+++ src/praktek1/anjing.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek1;
 
-/**
- *
- * @author Mamed
- */
 public class anjing {
+    String jenis;
+    String nama;
+    String asal;
+ 
     
+    void hewan(){
+        System.out.println("Jenis : "+jenis);
+        System.out.println("Nama : "+nama);
+        System.out.println("Asal : "+asal);
+}
 }
